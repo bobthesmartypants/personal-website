6d4afafe8cbb423118e558097a83c32ffe432cb7
Daniel Liu <hi.daniel.liu@gmail.com>
2021-06-01 18:50:18 -0700

    changed Navbar.css to Navbar.scss

--- src/Navbar/NavOp.js
+++ src/Navbar/NavOp.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import "./Navbar.css";
+import "./Navbar.scss";
 
 const NavOp = props => (
     <div
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './Navbar.css';
+import './Navbar.scss';
 
 import Logo from "./Logo";
 import NavOps from "./NavOps";
