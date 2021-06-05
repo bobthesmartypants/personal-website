37b734ffbd997a2e91cfd307744d1f85523c2f5e
Daniel Liu <hi.daniel.liu@gmail.com>
2021-06-05 01:50:19 -0700

    added picture to picture frame

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import "./About.css";
+import "../pointable.css";
+
+import polaroid from "../images/polaroid-frame.png";
+
 const About = () => {
-    return <h1>Hello World from About!</h1>;
+
+
+    return (
+        <div id="about-main">
+            <div id="picture-main">
+                <div id="picture-container">
+                    <div id="propic" />
+                    <img src={polaroid} alt="polaroid frame" id="picture-frame"/>
+                </div>
+
+
+            </div>
+            <div id="desc-main">
+                hihiihihihiihhihih
+
+            </div>
+        </div>
+    );
 };
 
 export default About;
